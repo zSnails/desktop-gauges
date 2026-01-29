b68d21d18af68206dd048b06d8d9c806b278486d
zSnails <erizojuan33@gmail.com>
2026-01-28 20:19:45 -0600

    feat: default colors for the gauge

---
+++ src/color.zig
@@ -0,0 +1,8 @@
+/// These are the default colors used for each gauge
+
+pub const RGB = struct { r: f64, g: f64, b: f64 };
+
+pub const CYBER_YELLOW = RGB{ .r = 1.0, .g = 0.9294117647, .b = 0.3058823529 };
+pub const CYBER_RED = RGB{ .r = 1, .g = 0.2196078431, .b = 0.3921568627 };
+pub const WHITE = RGB{ .r = 1, .g = 1, .b = 1 };
+pub const BLACK = RGB{ .r = 0, .g = 0, .b = 0 };
